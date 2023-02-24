1914c389b326ed56a4e48189392d586464c9e95e
Mahtab Mahdavi <mahtaabmahdavi@gmail.com>
2023-02-24 18:53:33 +0330

    Bugs were fixed

---
+++ Pong/README.md
@@ -0,0 +1,23 @@
+# ⚾ Pong 🏓
+
+This game is designed using [arcade](https://api.arcade.academy/en/latest/) library in python. Your opponent in this game is a simple artificial intelligence.
+
+At the beginning, your score and AI's score are zero. If the rival cannot catch your ball, your score will be increased by one, and if you cannot catch the competitor's ball, the AI score will be increased by one.
+
+---
+## Usage 🎮
+
+First install requirement package:
+```
+pip install arcade
+```
+Then you can run this program:
+```
+python main.py
+```
+**NOTE :** Your opponent's racket and yours only moves up and down. You can move your racket up and down by shaking the mouse.
+
+---
+## Game Environment:
+
+![Pong](Images/pong.jpg)
--- Pong/main.py
+++ Pong/main.py
@@ -1,64 +1,6 @@
-import random
 import arcade
-
-
-class Ball(arcade.Sprite):
-    def __init__(self, game):
-        # Properties
-        super().__init__()
-        self.center_x = game.width // 2
-        self.center_y = game.height // 2
-        self.color = arcade.color.YELLOW_ORANGE
-        self.radius = 15
-        self.change_x = random.choice([-1, 1])
-        self.change_y = random.choice([-1, 1])
-        self.width = self.radius * 2
-        self.height = self.radius * 2
-        self.speed = 5
-
-    # Methods
-    def move(self):
-        self.center_x += self.change_x * self.speed
-        self.center_y += self.change_y * self.speed
-
-    def draw(self):
-        arcade.draw_circle_filled(self.center_x, self.center_y, self.radius, self.color)
-
-
-class Rocket(arcade.Sprite):
-    def __init__(self, center_x, center_y, color, name):
-        # Properties
-        super().__init__()
-        self.center_x = center_x
-        self.center_y = center_y
-        self.color = color
-        self.name = name 
-        self.change_x = 0
-        self.change_y = 0
-        self.width = 10
-        self.height = 60
-        self.speed = 4
-        self.score = 0
-
-    # Methods
-    def move(self, game, ball):
-        if ball.center_x > (game.width // 2):
-            if self.center_y > ball.center_y:
-                self.change_y = -1
-
-            if self.center_y < ball.center_y:
-                self.change_y = 1
-
-            self.center_y += self.change_y * self.speed
-
-            if self.center_y < self.height:
-                self.center_y = self.height
-
-            if self.center_y < game.height - self.height:
-                self.center_y = game.height - self.height
-
-    def draw(self):
-        arcade.draw_rectangle_filled(self.center_x, self.center_y, self.width, self.height, self.color)
+from rocket import Rocket
+from ball import Ball
 
 
 class Game(arcade.Window):
@@ -66,8 +8,8 @@ class Game(arcade.Window):
         # Properties
         super().__init__(width = 800, height = 500, title = "Pong 🏓")
         arcade.set_background_color(arcade.color.DARK_GREEN)
-        self.player_1 = Rocket(40, self.height // 2, arcade.color.PINK_SHERBET, "Mahtab")
-        self.player_2 = Rocket(self.width - 40, self.height // 2, arcade.color.BABY_BLUE, "Artificial Intelligence")
+        self.player_1 = Rocket(40, self.height // 2, arcade.color.PINK_SHERBET, "You")
+        self.player_2 = Rocket(self.width - 40, self.height // 2, arcade.color.BABY_BLUE, "AI")
         self.ball = Ball(self)
 
     # Methods
@@ -75,6 +17,8 @@ class Game(arcade.Window):
         arcade.start_render()
         arcade.draw_rectangle_outline(self.width // 2, self.height // 2, self.width - 30, self.height - 30, arcade.color.WHITE, border_width = 10)
         arcade.draw_line(self.width // 2, 30, self.width // 2, self.height - 30, arcade.color.WHITE, line_width = 10)
+        arcade.draw_text(f"{self.player_1.name} : {self.player_1.score}", 30, self.height - 45, arcade.color.FLORAL_WHITE, width = 100, font_size = 14, align = "left")
+        arcade.draw_text(f"{self.player_2.name} : {self.player_2.score}", self.width - 130, self.height - 45, arcade.color.FLORAL_WHITE, width = 100, font_size = 14, align = "right")
         self.player_1.draw()
         self.player_2.draw()
         self.ball.draw()
@@ -90,15 +34,13 @@ class Game(arcade.Window):
 
         if self.ball.center_y < 30 or self.ball.center_y > self.height - 30:
             self.ball.change_y *= -1
-
         if arcade.check_for_collision(self.player_1, self.ball) or arcade.check_for_collision(self.player_2, self.ball):
             self.ball.change_x *= -1
-        
+            
         if self.ball.center_x < 0:
             self.player_2.score += 1
             del self.ball
             self.ball = Ball(self)
-
         if self.ball.center_x > self.width:
             self.player_1.score += 1
             del self.ball
